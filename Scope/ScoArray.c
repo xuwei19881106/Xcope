5bd50f643807fe9a0b69093ef42784dcf22772b0
xuwei1988 <xuwei1988@users.noreply.github.com>
2019-03-14 17:51:42 +0800

    Git Test

--- Scope/ScoArray.c
+++ Scope/ScoArray.c
@@ -2,15 +2,16 @@
 #include "ScoArray.h"
 #include "scopeport.h"
 #include "assertEx.h"
+/* 2019.03.14 */
 DEFINE_THIS_MODULE(ScoArray.c)
 
-/* 用于实时保存数据的全局数组 -------------------------------------------------*/
+/* 鐢ㄤ簬瀹炴椂淇濆瓨鏁版嵁鐨勫叏灞�鏁扮粍 -------------------------------------------------*/
 Uint16 scopeArray[SCOPE_MAX_CHN][SCOPE_CHN_PTS];
 
-/* 用于操作全局数组的控制块 ---------------------------------------------------*/
+/* 鐢ㄤ簬鎿嶄綔鍏ㄥ眬鏁扮粍鐨勬帶鍒跺潡 ---------------------------------------------------*/
 ScoArrayPCB scpAryPcb;
 
-/* 初始化函数，主要用于将内存置为0 --------------------------------------------*/
+/* 鍒濆鍖栧嚱鏁帮紝涓昏鐢ㄤ簬灏嗗唴瀛樼疆涓�0 --------------------------------------------*/
 void ScoArray_init(void) {
     bzero((Uint8 *)&scpAryPcb, sizeof(ScoArrayPCB));
     scpAryPcb.isFulled = false;
@@ -24,24 +25,24 @@ void ScoArray_init(void) {
 }
 
 Uint16 ScoArray_refresh(Uint16 *pdata, Uint8 len) {
-    /* step1: 先判断传入的数据长度是否合法 */
+    /* step1: 鍏堝垽鏂紶鍏ョ殑鏁版嵁闀垮害鏄惁鍚堟硶 */
     if(len != SCOPE_MAX_CHN) {
         return resRefreshErr;
     }
 
-    /* step2: 为每个通道更新数据*/
+    /* step2: 涓烘瘡涓�氶亾鏇存柊鏁版嵁*/
     int i;
     for(i = 0; i < SCOPE_MAX_CHN; i++){
         scopeArray[i][scpAryPcb.curPos] = *(pdata + i);
     }
 
-    /* step3: 更新数据并且检测是否需要环回 */
+    /* step3: 鏇存柊鏁版嵁骞朵笖妫�娴嬫槸鍚﹂渶瑕佺幆鍥� */
     if(++scpAryPcb.curPos == SCOPE_CHN_PTS){
         scpAryPcb.curPos = 0;
         scpAryPcb.isFulled = true;
     }
 
-    /* step4: 检测是否有事件发生并检测是否在此刻需要保存 */
+    /* step4: 妫�娴嬫槸鍚︽湁浜嬩欢鍙戠敓骞舵娴嬫槸鍚﹀湪姝ゅ埢闇�瑕佷繚瀛� */
 
 
     return 0;
@@ -49,11 +50,11 @@ Uint16 ScoArray_refresh(Uint16 *pdata, Uint8 len) {
 
 
 void ScoArray_channelLog(Uint8 chnID, Uint16 len) {
-    /* step1 先判断截取长度是否超限 */
+    /* step1 鍏堝垽鏂埅鍙栭暱搴︽槸鍚﹁秴闄� */
     ASSERT_EX(len <= SCOPE_CHN_PTS);
 
-    /* step2 拷贝相应通道数据 */
-    /* 为了防止数据回绕，应当分两部分COPY */
+    /* step2 鎷疯礉鐩稿簲閫氶亾鏁版嵁 */
+    /* 涓轰簡闃叉鏁版嵁鍥炵粫锛屽簲褰撳垎涓ら儴鍒咰OPY */
     Uint16 part1 = 0, part2 = 0;
     if(scpAryPcb.curPos < len) {
         part1 = len - scpAryPcb.curPos;
